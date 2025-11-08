e64a59e73663b2f34a90e5c32d217a70832c6a9c
IAN <IAN@LAPTOP-FKOTTQJK>
2025-11-07 20:16:05 -0500

    Proyecto Poo Estadio

---
+++ src/main/java/Negocio/AppEstadio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Negocio;
+
+/**
+ *
+ * @author IAN
+ */
+public class AppEstadio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
